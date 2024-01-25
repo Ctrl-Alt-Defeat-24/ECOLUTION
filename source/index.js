81c14b274919253fd06848caf36185252ba9609b
urban_r <riyaadshahid@gmail.com>
2024-01-25 22:11:27 +0000

    MongoDB setup (login/register progress)
    1. created a MongoDB database locally which the program can connect
    2. created basic login and register ejs posts
    tbc: code finds credentials in DB and matches end user's

--- source/index.js
+++ source/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 var ejs = require('ejs');
 var bodyParser= require ('body-parser');
+const bcrypt = require('bcrypt');
+const { MongoClient } = require('mongodb');
+const saltRounds = 10; // for bcrypt
 
 const electricitymaps = require('./apis/electricitymaps');
 const app = express();
 const port = 8000;
+// Connection URL (replace if your MongoDB is hosted elsewhere)
+const url = 'mongodb://127.0.0.1:27017';
 
 // Set the directory where Express will pick up HTML files
 // __dirname will get the current directory
@@ -21,8 +26,20 @@ app.engine('html', ejs.renderFile);
 // Static files are files that will not change
 app.use(express.static('public')); // Serve static files from the 'public' folder
 
+// Tell Express to use the body-parser middleware to parse request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 // Define our data
 var ecoData = {}
+
+// Database Name
+const dbName = 'Ecolution';
+
+let db;
+// Create a new MongoClient
+const client = new MongoClient(url);
 
 // Function to generate the chart
 app.get('/chart-data', (req, res) => {
@@ -36,6 +53,48 @@ app.get('/chart-data', (req, res) => {
 // Requires the main.js file inside the routes folder passing in the Express app and data as arguments.  All the routes will go in this file
 require("./js/main")(app, ecoData);
 
-app.listen(port, () => console.log(`ECOLUTION SVR LISTENING... PORT: ${port}!`));
+// Connect to the MongoDB cluster
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected successfully to server");
+        db = client.db(dbName);
+        const collection = db.collection('User_Credentials');
+
+        // Start the server after establishing the database connection
+        app.listen(port, () => {
+            console.log(`ECOLUTION SVR LISTENING... PORT: ${port}!`);
+        });
+
+        // Now you can require your routes and pass the db connection
+        require("./js/main")(app, ecoData, db, bcrypt, saltRounds, collection);
+
+    } catch (error) {
+        console.error("Failed to connect to MongoDB", error);
+    }
+}
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+  });
+
+app.post("/login", async (req, res) => {
+    // Authentication logic for login
+    const { username, password } = req.body;
+    const user = await db.collection('User_Credentials').findOne({ username });
+    
+    if (user) {
+        const match = await bcrypt.compare(password, user.password);
+        if (match) {
+            res.send("Logged in successfully");
+        } else {
+            res.send("Incorrect password");
+        }
+    } else {
+        res.send("User not found");
+    }
+  });
+
+run();
 
 let test = electricitymaps.getZoneID();
+
--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,45 @@
-module.exports = function (app, ecoData, chart) {
+module.exports = function (app, ecoData, db, bcrypt, saltRounds, collection) {
   // Handle our routes
   app.get("/", async (req, res) => {
-    const sampleData = { 
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'], 
-      values: [12, 19, 3, 5, 2, 3] 
-  };
-    res.render("index.ejs", { chart, ecoData});
+
+    res.render("index.ejs", {ecoData});
+  });
+//   app.get("/login", (req, res) => {
+//     res.render("login.ejs");
+//   });
+
+// app.post("/login", async (req, res) => {
+//     // Authentication logic for login
+//     const { username, password } = req.body;
+//     const user = await db.collection('User_Credentials').findOne({ username });
+    
+//     if (user) {
+//         const match = await bcrypt.compare(password, user.password);
+//         if (match) {
+//             res.send("Logged in successfully");
+//         } else {
+//             res.send("Incorrect password");
+//         }
+//     } else {
+//         res.send("User not found");
+//     }
+//   });
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
+  });
+
+app.post("/register", async (req, res) => {
+    // Logic to handle user registration
+    const { username, password } = req.body;
+    const existingUser = await db.collection('User_Credentials').findOne({ username });
+
+    if (existingUser) {
+        res.send("Username already taken");
+    } else {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        await db.collection('User_Credentials').insertOne({ username, password: hashedPassword });
+        res.send("Registered successfully");
+    }
   });
 };
