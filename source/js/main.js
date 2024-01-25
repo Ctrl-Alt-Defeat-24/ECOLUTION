afb97a22a1a25f6a1f805572fc7613c92c611a6f
Tahm24 <156429364+Tahm24@users.noreply.github.com>
2024-01-25 15:02:09 +0000

    chart display

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,6 @@
-module.exports = function (app, ecoData, chart) {
+module.exports = function (app, ecoData) {
   // Handle our routes
   app.get("/", async (req, res) => {
-    const sampleData = { 
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'], 
-      values: [12, 19, 3, 5, 2, 3] 
-  };
-    res.render("index.ejs", { chart, ecoData});
+    res.render("index.ejs", { ecoData });
   });
 };
