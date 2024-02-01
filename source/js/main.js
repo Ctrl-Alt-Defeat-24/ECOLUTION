db71d3d54c417e361454fcfd96916af869aba966
PRIMΛL <66prg99@gmail.com>
2024-02-01 12:19:36 +0000

    Squashed commit of the following:
    commit 51969484534fc2c44598f2b8de9fa4cd0212f946
    Merge: 81c14b2 afb97a2
    Author: urban_r <riyaadshahid@gmail.com>
    Date:   Thu Jan 25 22:13:36 2024 +0000
    
        Merge branch 'chart-test' of https://github.com/Ctrl-Alt-Defeat-24/ECOLUTION into chart-test
    
    commit 81c14b274919253fd06848caf36185252ba9609b
    Author: urban_r <riyaadshahid@gmail.com>
    Date:   Thu Jan 25 22:11:27 2024 +0000
    
        MongoDB setup (login/register progress)
    
        1. created a MongoDB database locally which the program can connect
        2. created basic login and register ejs posts
        tbc: code finds credentials in DB and matches end user's
    
    commit afb97a22a1a25f6a1f805572fc7613c92c611a6f
    Author: Tahm24 <156429364+Tahm24@users.noreply.github.com>
    Date:   Thu Jan 25 15:02:09 2024 +0000
    
        chart display
    
    commit fcf8cd6415e620214874181e027b955cd93d801f
    Author: urban_r <riyaadshahid@gmail.com>
    Date:   Fri Jan 19 12:14:44 2024 +0000
    
        chart not displaying
    
        Tryed adding a bar chart to represent API data using billboard.js but dont have a working model yet

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,48 @@
 const ecolutionTravelRoutes = require("./travelroutes");
 
-module.exports = function (app, ecoData) {
+module.exports = function (app, ecoData, db, bcrypt, saltRounds, collection) {
   // Handle our routes
-  app.get("/", (req, res) => {
-    res.render("index.ejs", ecoData);
+  app.get("/", async (req, res) => {
+
+    res.render("index.ejs", {ecoData});
+  });
+//   app.get("/login", (req, res) => {
+//     res.render("login.ejs");
+//   });
+
+// app.post("/login", async (req, res) => {
+//     // Authentication logic for login
+//     const { username, password } = req.body;
+//     const user = await db.collection('User_Credentials').findOne({ username });
+    
+//     if (user) {
+//         const match = await bcrypt.compare(password, user.password);
+//         if (match) {
+//             res.send("Logged in successfully");
+//         } else {
+//             res.send("Incorrect password");
+//         }
+//     } else {
+//         res.send("User not found");
+//     }
+//   });
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
+  });
+
+app.post("/register", async (req, res) => {
+    // Logic to handle user registration
+    const { username, password } = req.body;
+    const existingUser = await db.collection('User_Credentials').findOne({ username });
+
+    if (existingUser) {
+        res.send("Username already taken");
+    } else {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        await db.collection('User_Credentials').insertOne({ username, password: hashedPassword });
+        res.send("Registered successfully");
+    }
   });
 
   app.get("/directions", (req, res) => {
